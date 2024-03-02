55e60d8e3f5e6435b36416748f73aeaba066bba2
name <email>
2024-03-02 20:53:10 +0530

    variables in java

---
+++ variables/IntDemo.java
@@ -0,0 +1,8 @@
+public class IntDemo {
+    public static void main(String[] args) {
+        String name="Ravi";
+        int age = 20;
+        System.out.println("Hello" + " " +name +" Your age is "+ " " +age);
+    }
+    
+}
