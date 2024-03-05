3179027822af3b85c9530f727173217deb632b72
name <email>
2024-03-05 22:02:11 +0530

    max byte and min byte value

---
+++ Datatypes/maxbyte_minbyte.java
@@ -0,0 +1,9 @@
+public class maxbyte_minbyte {
+    public static void main(String[] args) {
+        byte max= Byte.MAX_VALUE;
+        byte min = Byte.MIN_VALUE;
+
+        System.out.println("The max byte value is:"+max);
+        System.out.println("The min byte value is :"+min);
+    }
+}
