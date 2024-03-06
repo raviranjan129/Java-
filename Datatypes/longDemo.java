1e8dcf2a8df31da8a168017e2c130ddabb72805a
name <email>
2024-03-06 09:11:31 +0530

    data types

---
+++ Datatypes/longDemo.java
@@ -0,0 +1,12 @@
+
+import java.util.*;;
+public class longDemo {
+    public static void main(String[] args) {
+         long maxvalue = Long.MAX_VALUE;
+         Long minvalue = Long.MIN_VALUE;
+
+         System.out.println("The max long value is :"+maxvalue);
+         System.out.println("The min long value is :"+minvalue);
+    }
+    
+}
---
+++ Datatypes/shortDemo.java
@@ -0,0 +1,10 @@
+ import java.util.*;;
+public class shortDemo {
+     public static void main(String [] args){
+        short MaxNumber = Short.MAX_VALUE;
+        short minNumber= Short.MIN_VALUE;
+
+        System.out.println("Maximum short number ="+MaxNumber);
+        System.out.println("Minimum short number ="+minNumber);
+     }
+}
